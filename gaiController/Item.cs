749a6c837add8ba746a5875ff37fd77906c728f0
Trickermand <Patrickhansen2500@hotmail.com>
2017-11-15 22:25:10 +0100

    Added easter eggs. Additional border stuff.

---
+++ gaiController/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gaiController
+{
+    public class Item
+    {
+        public string name;
+        public string itemColor;
+        public bool isMag;
+        public bool isPhy;
+        public bool isKatana;
+        
+        public Item(string name, string itemColor, bool isMag, bool isPhy, bool isKatana)
+        {
+            this.name = name;
+            this.itemColor = itemColor;
+            this.isMag = isMag;
+            this.isPhy = isPhy;
+            this.isKatana = isKatana;
+        }
+
+    }
+    
+}
+
